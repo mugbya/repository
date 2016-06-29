c3ef62df13a9e557e8ab28176cfbc1da16ee13b7
mugbya <mugbya@gmail.com>
2016-06-29 21:10:21 +0800

    接入项目

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^list', views.list, name='list'),
-    #
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+    url(r'^(?P<pk>[A-Fa-f0-9]{8}-([A-Fa-f0-9]{4}-){3}[A-Fa-f0-9]{12})$', views.DetailView.as_view(), name='detail'),
+
+
+    url(r'^new$', views.new_blog, name='new_blog'),
+
     # # url(r'^settings$', views.settings, name='settings'),
     # #
     # # url(r'^uploadavatar_upload$', views.uploadavatar_upload, name='uploadavatar_upload'),
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.views import generic
 
-# Create your views here.
+from .models import Blog
 
 
-'''
-all blogs list
-'''
-def list(request):
-    return render(request, 'blog/list.html',)
+class IndexView(generic.ListView):
+    '''
+    all blog list
+    '''
+    template_name = 'blog/index.html'
+    # context_object_name = 'blog_list'
 
+    def get_queryset(self):
+        return Blog.objects.order_by('-published_date')[:10]
+
+
+class DetailView(generic.DetailView):
+    model = Blog
+    template_name = 'blog/detail.html'
 
 '''
 user blog list
 '''
 def blogsList(request):
     return  render(request, 'blog/blogList.html', )
+
+
+@login_required()
+def new_blog(request):
+    return  render(request, 'blog/edit.html', )
+    # if request.method == "POST":
+    #     form = QuestionForm(request.POST)
+    #     if form.is_valid():
+    #         form.clean()
+    #         post = form.save(commit=False)
+    #         post.author = request.user
+    #         if 'publish' in request.POST:
+    #             post.publish()
+    #             # return redirect('qs.views.index')
+    #             return redirect(reverse_lazy('index'))
+    #         else:
+    #             post.save()
+    #             return redirect('qs.views.draft_list')
+    # else:
+    #     form = QuestionForm()
+    # return render(request, 'qs/edit.html', {'form': form})
